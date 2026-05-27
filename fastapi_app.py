5250af04389edbbc2a0818ddaff57cef291d8699
Husen <kasabhusen5@gmail.com>
2026-05-27 21:29:20 +0530

    Fix FastAPI app

--- fastapi_app.py
+++ fastapi_app.py
@@ -1,46 +1,58 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import sqlite3
 
 app = FastAPI()
 
+# Static folder
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Templates folder
 templates = Jinja2Templates(directory="templates")
 
+# Database setup
+conn = sqlite3.connect("todos.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS todos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    task TEXT
+)
+""")
+conn.commit()
+
+
+# Home page
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
+    cursor.execute("SELECT * FROM todos")
+    todos = cursor.fetchall()
+
     return templates.TemplateResponse(
         "index.html",
-        {"request": request}
-    )
+        {
+            "request": request,
             "todos": todos
         }
     )
 
 
+# Add task
 @app.post("/add")
 async def add(task: str = Form(...)):
-
-    cursor.execute(
-        "INSERT INTO todos (task) VALUES (?)",
-        (task,)
-    )
-
+    cursor.execute("INSERT INTO todos (task) VALUES (?)", (task,))
     conn.commit()
 
     return RedirectResponse("/", status_code=303)
 
 
+# Delete task
 @app.get("/delete/{todo_id}")
 async def delete(todo_id: int):
-
-    cursor.execute(
-        "DELETE FROM todos WHERE id=?",
-        (todo_id,)
-    )
-
+    cursor.execute("DELETE FROM todos WHERE id = ?", (todo_id,))
     conn.commit()
 
     return RedirectResponse("/", status_code=303)
